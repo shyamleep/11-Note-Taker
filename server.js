d7eed27634f1616aad6d319029ef7539fa106be8
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2020-12-31 17:14:25 -0500

    server.js

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 const express = require("express");
-const fs = require("fs")
+const fs = require("fs");
+const apiRoutes = require("./routes/api");
+const htmlRoutes = require("./routes/html");
+
+// create express server
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware for data parsing
+app.use(express.static("public"));
+app.use(express.json);
+app.use(express.urlencoded({extended: true}));
+
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => console.log("listening on PORT: " + PORT))
