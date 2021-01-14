cc64d0c71342221afd23c288ffe8d1e6e9fbb46f
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-14 01:41:57 -0500

    made a store.js

---
+++ db/store.js
@@ -0,0 +1,29 @@
+const util = require("util");
+const fs = require("fs");
+const uuidv1 = require("uuid/v1");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync("db/db.json", "utf8");
+    };
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note));
+    };
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes; 
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            };
+            return parsedNotes
+        });
+    };
+    addNote(note) {
+        
+    }
+}
